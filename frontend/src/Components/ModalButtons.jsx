6a37d0651ad87b1af62b34c706e717a6e7e884d8
Johan Gericke <johan.gericke@voxtelecom.co.za>
2024-03-27 19:32:32 +0200

    Adding redis to the backend for convenient caching of the data

--- frontend/src/Components/ModalButtons.jsx
+++ frontend/src/Components/ModalButtons.jsx
@@ -13,6 +13,48 @@ const forms = {
 }
 
 
+function IncomeForm({input, handleChange}) {
+    return (
+        <Form>
+            <Form.Group className='mb-3' controlId='formFinancialData'>
+                <Form.Label>Income name</Form.Label>
+                <Form.Control type='text' name='incname' placeholder='Salary/Investment/Other' onChange={handleChange} />
+                <Form.Label>Income amount</Form.Label>
+                <Form.Control type='text' name='incamount' placeholder='Enter Income Amount'  onChange={handleChange} />
+                <Form.Label>Income type</Form.Label>
+                <Form.Select aria-label="inctype" onChange={handleChange}>
+                    <option value="">Select Income Type</option>
+                    <option value="1">Salary</option>
+                    <option value="2">Investment</option>
+                    <option value="3">Other</option>
+                </Form.Select>
+            </Form.Group>
+        </Form>
+    );
+}
+
+
+function AccountForm({input, handleChange}) { 
+    return (
+        <Form name='accform'>
+            <Form.Group className='mb-3' controlId='formFinancialData'>
+                <Form.Label>Card name</Form.Label>
+                <Form.Control type='text' name='cardname' placeholder='Cheque/Savings/Credit Account' onChange={handleChange} />
+                <Form.Label>Card number</Form.Label>
+                <Form.Control type='text' name='cardnum' placeholder='Enter Card Number' onChange={handleChange}/>
+                <Form.Label>Card type</Form.Label>
+                <Form.Select aria-label="acctype" name='cardtype' onChange={handleChange}>
+                    <option value="">Select Card Type</option>
+                    <option value="1">Cheque</option>
+                    <option value="2">Savings</option>
+                    <option value="3">Credit</option>
+                </Form.Select>
+            </Form.Group>
+        </Form>
+    );
+}
+
+
 function isValidAccountNumber(accountNumber) {
     // Regular expression pattern for a valid account number
     const accountNumberPattern = /^[0-9]{6,}$/;
@@ -37,49 +79,8 @@ function isValidAccountNumber(accountNumber) {
         accountType = 'Unknown';
     }
 
-    return {isValidAccountNumber: accountNumberPattern.test(accountNumber), accType: accountType};
+    return {validAccNumber: accountNumberPattern.test(accountNumber), accType: accountType};
 }
-
-
-function IncomeForm({input, handleChange}) {
-    return (
-        <Form>
-            <Form.Group className='mb-3' controlId='formFinancialData'>
-                <Form.Label>Income name</Form.Label>
-                <Form.Control type='text' name='incname' placeholder='Salary/Investment/Other' onChange={handleChange} />
-                <Form.Label>Income amount</Form.Label>
-                <Form.Control type='text' name='incamount' placeholder='Enter Income Amount'  onChange={handleChange} />
-                <Form.Label>Income type</Form.Label>
-                <Form.Select aria-label="inctype" onChange={handleChange}>
-                    <option value="1">Salary</option>
-                    <option value="2">Investment</option>
-                    <option value="3">Other</option>
-                </Form.Select>
-            </Form.Group>
-        </Form>
-    );
-}
-
-
-function AccountForm({input, handleChange}) { 
-    return (
-        <Form name='accform'>
-            <Form.Group className='mb-3' controlId='formFinancialData'>
-                <Form.Label>Card name</Form.Label>
-                <Form.Control type='text' name='cardname' placeholder='Cheque/Savings/Credit Account' onChange={handleChange} />
-                <Form.Label>Card number</Form.Label>
-                <Form.Control type='text' name='cardnum' placeholder='Enter Card Number' onChange={handleChange} />
-                <Form.Label>Card type</Form.Label>
-                <Form.Select aria-label="acctype" name='cardtype' onChange={handleChange}>
-                    <option value="1">Cheque</option>
-                    <option value="2">Savings</option>
-                    <option value="3">Credit</option>
-                </Form.Select>
-            </Form.Group>
-        </Form>
-    );
-}
-
 
 function CreateButton (opts) {
     const [ show, setShow ] = useState(false);
@@ -91,11 +92,27 @@ function CreateButton (opts) {
     const [input, setInput] = useState({});
 
     const handleChange = (ev) => {
-        console.log(ev.target.name);
-        setInput({
-            ...input,
-            [ev.target.name]: ev.target.value
-        });
+        if (ev.target.name === 'cardnum') {
+            const {validAccNumber, accType} = isValidAccountNumber(ev.target.value);
+            if (!validAccNumber) {
+                ev.target.classList.add('invalid-account-number'); // Add CSS class for invalid account number
+                setInput({
+                    ...input,
+                    [ev.target.name]: ev.target.value
+                });
+            } else {
+                ev.target.classList.remove('invalid-account-number'); // Remove CSS class for valid account number
+                setInput({
+                    ...input,
+                    [ev.target.name]: ev.target.value
+                });
+            }
+        } else {
+            setInput({
+                ...input,
+                [ev.target.name]: ev.target.value
+            });
+        }
     }
 
     const handleSubmit = (ev) => {
@@ -107,8 +124,8 @@ function CreateButton (opts) {
         .then((res) => {
             console.log(res);
             console.log(res.data);
-            handleClose();
         });
+        handleClose();
     }
 
 
